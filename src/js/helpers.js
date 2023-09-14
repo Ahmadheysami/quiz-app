5b04bd3d0d2bb349814a9d0c1a7d71d2fe651938
Ahmadheysami <alheythami@gmail.com>
2023-09-14 00:11:15 -0700

    add and remove visible class

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,10 +2,12 @@ import { loading } from "./temlates";
 
 function show(el, display) {
   el.style.display = display;
+  el.classList.add("visible");
 }
 
 function hide(el) {
   el.style.display = "none";
+  el.classList.remove("visible");
 }
 
 function createLoading(parent) {
